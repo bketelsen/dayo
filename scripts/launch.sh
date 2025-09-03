193c104c5f3e003a4bb78520c7af90226563419b
Brian Ketelsen <bketelsen@gmail.com>
2025-09-03 07:19:59 -0400

    fix: trixie with systemd:obs

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,5 +6,5 @@ incus config device override dayo root size=50GiB
 incus config set dayo limits.cpu=4 limits.memory=8GiB
 incus config set dayo security.secureboot=false
 
-incus config device add dayo install disk source=/home/bjk/projects/mkosistuff/dayo/mkosi.output/DayoServer_202509030044_x86-64.raw boot.priority=90
+incus config device add dayo install disk source=/home/bjk/projects/mkosistuff/dayo/mkosi.output/DayoServer_202509031113_x86-64.raw boot.priority=90
 incus start dayo --console=vga
