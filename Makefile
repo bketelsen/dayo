3def640f933ca3e1f93ecc5d816a2aac9e3d63b5
Brian Ketelsen <bketelsen@gmail.com>
2025-09-02 21:12:21 -0400

    fix: bootable

--- Makefile
+++ Makefile
@@ -11,3 +11,12 @@ clean:
 
 iso:
 	sudo ./scripts/convert-img-to-iso.sh mkosi.output/DayoServerInstaller_202508262004_x86-64.raw
+
+start:
+	incus start dayo || true
+
+console: start
+	incus console --type=vga dayo
+
+dd:
+	mkosi burn --force /dev/nvme1n1
---
+++ scripts/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+incus init dayo --empty --vm
+incus config device override dayo root size=50GiB
+incus config set dayo limits.cpu=4 limits.memory=8GiB
+incus config set dayo security.secureboot=false
+
+incus config device add dayo install disk source=/home/bjk/projects/mkosistuff/dayo/mkosi.output/DayoServer_202509030044_x86-64.raw boot.priority=90
+incus start dayo --console=vga
